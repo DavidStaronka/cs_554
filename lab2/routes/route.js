21c85a74f4a70d9be62e3400923791033ec2112f
David Staronka <dstaronk@gmail.com>
2022-02-01 19:31:43 -0500

    passthrough to WSL

--- lab2/routes/route.js
+++ lab2/routes/route.js
@@ -10,26 +10,6 @@ let recentVisits = [];
 bluebird.promisifyAll(redis.RedisClient.prototype);
 bluebird.promisifyAll(redis.Multi.prototype);
 
-function type_checker(item, type, errString, objType){
-    if(item == undefined || typeof(item) != type || item.length == 0) throw errString;
-    if(type == "string"){
-        //got this if statement from stack overflow here: https://stackoverflow.com/questions/2031085/how-can-i-check-if-string-contains-characters-whitespace-not-just-whitespace
-        if (!/\S/.test(item)) throw errString;
-    }
-    if(objType == "array"){
-        if(!Array.isArray(item) || item.length == 0) throw errString;
-    }
-}
-
-function stringCheck(str) {
-    return typeof str === 'string' && str.length > 0 && str.replace(/\s/g, "").length > 0;
-}
-
-function validateId(id) {
-    if(!id || typeof id !== 'string' || !stringCheck(id) || !ObjectId.isValid(id))
-        throw new Error('id must be a valid ObjectId string');
-}
-
 router.use('/history', async (req, res) => {
     try{
         res.send(recentVisits.slice(0, 21));
